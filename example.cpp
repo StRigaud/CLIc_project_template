c234134c1e4c5be59a30004df29d4e17b44951be
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2020-10-26 13:00:38 +0100

    add cmakelist, src, and license

---
+++ example.cpp
@@ -0,0 +1,25 @@
+/*  CLIc example source file - Copyright 2020 Stéphane Rigaud, Robert Haase,
+*   Institut Pasteur Paris, Max Planck Institute for Molecular Cell Biology and Genetics Dresden
+*
+*   CLIc is part of the clEsperanto project http://clesperanto.net 
+*
+*/
+
+
+#include "CLE.h"
+
+/**
+ * Main test function
+ *
+ */
+int main(int argc, char **argv)
+{
+    
+
+    /**
+     * Paste your code here :)
+     */
+
+    // That's all folks!
+    return EXIT_SUCCESS;
+}
